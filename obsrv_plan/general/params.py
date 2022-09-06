14f48609b806447126d526a6e9f26abedb247fd8
KnightIV <ramon.caballeroiv@gmail.com>
2022-09-05 19:15:20 -0500

    Added working directory for running in linux

--- obsrv_plan/general/params.py
+++ obsrv_plan/general/params.py
@@ -5,7 +5,12 @@ General use variables; SIMBAD query URL, result directory, observation datetime
 from datetime import datetime, date, time, timedelta, timezone
 from os.path import join
 
-WORKING_DIR = r"C:\Users\Ramon\Documents\Uni\Tesis\CVs Data"
+# Windows
+# WORKING_DIR = r"C:\Users\Ramon\Documents\Uni\Tesis\CVs Data"
+
+# Fedora
+WORKING_DIR = "/home/ramon/uanl/Tesis/UANL_MAPTA_PlanObservaciones"
+
 DATA_FILE_PATH = join(WORKING_DIR, "sdssdr9_exclusion-color_filter-variable-gdr2-j2000-result.csv")
 TIMEZONE = timezone(timedelta(hours=-5))
 OBSV_START_DATETIME = datetime.combine(
