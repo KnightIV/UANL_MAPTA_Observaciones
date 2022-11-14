88987a55934665eca94b6c9c143a1db9dcb2ba36
KnightIV <ramon.caballeroiv@gmail.com>
2022-11-14 13:30:53 -0600

    Light curve plot changes Changed color of scatter plots to match representation of Gaia bands Fixed issue where it failed to create subplots for a single source

--- obsrv_plan/gaia/photometry.py
+++ obsrv_plan/gaia/photometry.py
@@ -1,16 +1,23 @@
 import modin.pandas as pd
 import matplotlib.pyplot as plt
 
-PHOTOMETRY_CSV_PATH = r"/run/media/ramon/USBD/Tesis/Gaia Analisis/Gaia Light Curves/EPOCH_PHOTOMETRY-Gaia DR3 1982536326650942336.csv"
+# PHOTOMETRY_CSV_PATH = r"/run/media/ramon/USBD/Tesis/Gaia Analisis/Gaia Light Curves/EPOCH_PHOTOMETRY-Gaia DR3 1982536326650942336.csv"
+PHOTOMETRY_CSV_PATH = r"/run/media/ramon/USBD/Tesis/Gaia Analisis/Gaia Light Curves/rcabal011668446282116/EPOCH_PHOTOMETRY_COMBINED.csv"
 
-def plotBand(source_id, bands, vals, axsRow):
+def plotBand(source_id, vals, axsRow):
+	BAND_COLORS = {
+		'G': 'green',
+		'BP': 'blue',
+		'RP': 'red'
+	}
+
 	axsRow[0].set_title(f"{source_id} Magnitude")
 	axsRow[1].set_title(f"{source_id} Flux")
 
-	for band in bands:
+	for band in BAND_COLORS:
 		filteredBand = vals[vals['band'] == band]
-		axsRow[0].scatter(filteredBand['time'], filteredBand['mag'], label=f"{band}")
-		axsRow[1].scatter(filteredBand['time'], filteredBand['flux'], label=f"{band}")
+		axsRow[0].scatter(filteredBand['time'], filteredBand['mag'], label=f"{band}", color=BAND_COLORS[band])
+		axsRow[1].scatter(filteredBand['time'], filteredBand['flux'], label=f"{band}", color=BAND_COLORS[band])
 	
 	axsRow[0].legend()
 
@@ -19,8 +26,9 @@ if __name__ == "__main__":
 	df = df[df['rejected_by_photometry'] == False]
 
 	source_ids = df['source_id'].unique()
+	print(source_ids)
+
 	sourceIdCount = len(source_ids)
-	bands = df['band'].unique()
 
 	fig, axs = plt.subplots(2, sourceIdCount)
 	fig.suptitle(f"GDR3 Light Curves")
@@ -34,6 +42,6 @@ if __name__ == "__main__":
 		else:
 			axsRow = axs
 		sourceVals = df[df['source_id'] == source_ids[i]]
-		plotBand(source_id, bands, sourceVals, axsRow)
+		plotBand(source_id, sourceVals, axsRow)
 
 	plt.show()
