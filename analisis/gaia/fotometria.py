8d4c68da2c8c17628c298a4fbdd6ee644d2b1ab9
KnightIV <ramon.caballeroiv@gmail.com>
2022-12-12 14:46:09 -0600

    Photometry processing Moved Gaia photometry processing to new separate module "analisis" Notebook for processing images from Iturbide

--- analisis/gaia/fotometria.py
+++ analisis/gaia/fotometria.py
@@ -2,7 +2,7 @@ import modin.pandas as pd
 import matplotlib.pyplot as plt
 
 # PHOTOMETRY_CSV_PATH = r"/run/media/ramon/USBD/Tesis/Gaia Analisis/Gaia Light Curves/EPOCH_PHOTOMETRY-Gaia DR3 1982536326650942336.csv"
-PHOTOMETRY_CSV_PATH = r"/run/media/ramon/USBD/Tesis/Gaia Analisis/Gaia Light Curves/rcabal011668446282116/EPOCH_PHOTOMETRY_COMBINED.csv"
+PHOTOMETRY_CSV_PATH = "/run/media/ramon/USBD/Tesis/Gaia Analisis/Gaia Light Curves/rcabal011668446868155/EPOCH_PHOTOMETRY-Gaia DR3 1982536257931470464.csv"
 
 def plotBand(source_id, vals, axsRow):
 	BAND_COLORS = {
