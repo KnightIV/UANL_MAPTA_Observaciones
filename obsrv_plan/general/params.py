8c5dd9321dfa06400ad19dce6424bc186fff7c1e
KnightIV <ramon.caballeroiv@gmail.com>
2023-10-18 10:42:04 -0600

    Re-ran observation script with new data

--- obsrv_plan/general/params.py
+++ obsrv_plan/general/params.py
@@ -11,12 +11,15 @@ from multiprocessing import cpu_count
 
 # Fedora
 # WORKING_DIR = "/home/ramon/uanl/Tesis/UANL_MAPTA_PlanObservaciones"
-WORKING_DIR = "/run/media/ramon/T7/Tesis/Gaia Analisis";
+# WORKING_DIR = "/run/media/ramon/T7/Tesis/Gaia Analisis";
+WORKING_DIR = "/home/ramon/uanl/Tesis/Backup Data/Gaia Query Results/sdss-half"
 
-DATA_FILE_PATH = join(WORKING_DIR, "gdr3_sdss_exclusion_phot_cv_candidates-result.csv")
+# DATA_FILE_PATH = join(WORKING_DIR, "gdr3_sdss_exclusion_phot_cv_candidates-result.csv")
+DATA_FILE_PATH = join(WORKING_DIR, "SDSS Half Photometric Filter GDR3 - Limit-result.csv")
+
 TIMEZONE = timezone(timedelta(hours=-6))
 OBSV_START_DATETIME = datetime.combine(
-		date(year=2022, month=12, day=9), time(hour=23, minute=0, tzinfo=TIMEZONE))
+		date(year=2022, month=12, day=6), time(hour=23, minute=0, tzinfo=TIMEZONE))
 RESULT_DIR = join(WORKING_DIR, OBSV_START_DATETIME.strftime("%Y%m%d"))
 
 SIMBAD_VOT_RESULTS = join(RESULT_DIR, "simbad-raw_vot")
