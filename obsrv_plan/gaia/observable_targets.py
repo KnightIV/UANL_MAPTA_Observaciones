5649cf5d7b47ac838f7cdb9335716b343634ed7b
KnightIV <ramon.caballeroiv@gmail.com>
2024-04-23 10:43:31 -0600

    Re-worked so that elements are now read in line by line Memory saving; doesn't try to use all RAM + swap anymore Significant speed-up due to not having to read in all data

--- obsrv_plan/gaia/observable_targets.py
+++ obsrv_plan/gaia/observable_targets.py
@@ -2,13 +2,18 @@
 Read in Gaia candidates from CSV file and determine which objects from Gaia are observable on the night in question.
 """
 
-from obsrv_plan.general.params import TIMEZONE, OBSV_START_DATETIME, DATA_FILE_PATH, RESULT_DIR, MAX_PARALLEL
+from obsrv_plan.general.params import TIMEZONE, OBSV_START_DATETIME, DATA_FILE_PATH, RESULT_DIR, MAX_PARALLEL, WORKING_DIR
 from obsrv_plan.decorators.static_decorator import static_vars
 from obsrv_plan.general.log import printToLog
 
 import os
+import csv
+
 import numpy as np
+
 import pandas as pd
+import dask.dataframe as dd
+
 import astropy.units as u
 from math import ceil, floor
 from datetime import timedelta
@@ -18,8 +23,7 @@ from astroplan import Observer
 from astropy.coordinates import SkyCoord
 from astropy.time import Time, TimeDelta
 
-@static_vars(obsv=Observer(longitude=-99.895328*u.deg, latitude=24.75521 * u.deg, 
-						elevation=2400*u.m, name="Iturbide", timezone=TIMEZONE))
+@static_vars(obsv=Observer(longitude=-99.895328*u.deg, latitude=24.75521 * u.deg, elevation=2400*u.m, name="Iturbide", timezone=TIMEZONE))
 def __is_observable(ra, dec, g_mag):
 	ITURBIDE_OBSV = __is_observable.obsv
 	FMT_STR = "%Y-%m-%dT%H:%M:%S"
@@ -39,25 +43,34 @@ def __is_observable(ra, dec, g_mag):
 			ITURBIDE_OBSV.is_night(time=mer_time, horizon=-6 * u.deg) and
 				ITURBIDE_OBSV.target_is_up(target=target, time=mer_time, horizon=-6 * u.deg) and
 				np.any(ideal_mer_time.isclose(other=mer_time, atol=MER_TOLERANCE)) and
-				g_mag <= 16)
+				g_mag <= 15)
 
-def __processSubset(df):
+def __processSubset(df_path: str):
 	pid = os.getpid()
-	rowsToProcess = len(df)
-	printToLog(f"{pid}: Begin processing {rowsToProcess} rows")
+	printToLog(f"{pid}: Begin processing {df_path}", print_console=True)
+	
+	rowsProcessed = 0
+	obsv_targets: pd.DataFrame
+	with open(df_path, 'r') as csv_file:
+		reader = csv.reader(csv_file)
+		header = next(reader)
+		RA_INDEX = header.index('J2000_ra_prop')
+		DEC_INDEX = header.index('J2000_dec_prop')
+		G_MAG_INDEX = header.index('phot_g_mean_mag')
+		columns = header
 
-	for i in df.index:
-		if rowsToProcess % 100 == 0:
-			print(f"{pid}: {rowsToProcess} elements left to process")
-		ra = df.loc[i, 'J2000_ra_prop']
-		dec = df.loc[i, 'J2000_dec_prop']
-		g_mag = df.loc[i, 'phot_g_mean_mag']
-		(mer_time, can_obsv) = __is_observable(ra, dec, g_mag)
-		df.loc[i, 'mer_time'] = mer_time
-		df.loc[i, 'can_obsv'] = can_obsv
-		rowsToProcess -= 1
+		obsv_targets = pd.DataFrame(columns=columns)
+		for row in reader:
+			if rowsProcessed % 100 == 0:
+				print(f"{pid}: {rowsProcessed} rows processed")
+			(mer_time, can_obsv) = __is_observable(float(row[RA_INDEX]), float(row[DEC_INDEX]), float(row[G_MAG_INDEX]))
+			if can_obsv:
+				row[-1] = mer_time
+				row[-2] = can_obsv
+				obsv_targets = pd.concat([obsv_targets, pd.DataFrame([row], columns=columns)], ignore_index=True)
+			rowsProcessed += 1
+	obsv_targets = obsv_targets.apply(pd.to_numeric, errors='ignore')
 
-	obsv_targets = df[df.can_obsv]
 	obsv_targets = obsv_targets.sort_values(by=['dec'])
 	print(f"{pid}: {len(obsv_targets)} objects visible found")
 	return obsv_targets
@@ -66,28 +79,29 @@ def __exportTargets(obsv_targets: pd.DataFrame):
 	min_dec = obsv_targets.iloc[0]['dec']
 	max_dec = obsv_targets.iloc[-1]['dec']
 	output_file_path = os.path.join(RESULT_DIR, f"dec_{floor(min_dec)}_{ceil(max_dec)}.csv")
-	obsv_targets.to_csv(output_file_path)
+	obsv_targets.to_csv(output_file_path, index=False)
 	printToLog(f"Results output to {output_file_path}")
 
 def gaiaObservableTargets():
-	if not os.path.exists(RESULT_DIR):
-		os.makedirs(RESULT_DIR)
+	os.makedirs(RESULT_DIR, exist_ok=True)
+	daskSplitsDir = os.path.join(WORKING_DIR, "dask-splits")
 
-	df = pd.read_csv(DATA_FILE_PATH)
-	print("Finished reading in Gaia CSV data.")
-	# df = df.sample(20000)
-	df = df.assign(can_obsv=lambda _: False, mer_time=lambda _: None)
+	if not os.path.exists(daskSplitsDir):
+		df = dd.read_csv(DATA_FILE_PATH).repartition(npartitions=MAX_PARALLEL)
+		# df = df.sample(frac=0.005)
+		num_rows = len(df)
+		print(f"Rows to process: {num_rows}")
+		df = df.assign(can_obsv=lambda _: False, mer_time=lambda _: None)
+		os.makedirs(daskSplitsDir, exist_ok=True)
+		dd.to_csv(df, os.path.join(WORKING_DIR, "dask-splits", "*.csv"))
 
-	num_rows = len(df)
-	print(f"Rows to process: {num_rows}")
-
-	obsv_targets: pd.DataFrame
+	obsv_targets: dd.DataFrame
 	with Pool(MAX_PARALLEL) as p:
-		obsv_targets = pd.concat(p.map(__processSubset, np.array_split(df, MAX_PARALLEL)))
+		obsv_targets = pd.concat(p.map(__processSubset, [os.path.join(daskSplitsDir, f) for f in os.listdir(daskSplitsDir)]))
 	print(f"Found total {len(obsv_targets)} visible objects")
 	obsv_targets = obsv_targets.sort_values(by=['dec'])
-
-	[__exportTargets(tgts) for tgts in np.array_split(obsv_targets, MAX_PARALLEL)]
+	for tgts in np.array_split(obsv_targets, MAX_PARALLEL):
+		__exportTargets(tgts)	
 
 if __name__ == "__main__":
 	gaiaObservableTargets()
