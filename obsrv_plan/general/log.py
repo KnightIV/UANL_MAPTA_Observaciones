4fe12e614421cd4a7b51eac6d5be3f8a747592f7
KnightIV <ramon.caballeroiv@gmail.com>
2024-04-23 10:42:06 -0600

    QoL and re-ran for 04/26 weekend

--- obsrv_plan/general/log.py
+++ obsrv_plan/general/log.py
@@ -7,10 +7,13 @@ from multiprocessing import Lock
 
 LOGGING_LOCK = Lock()
 
-def printToLog(msg: str):
+def printToLog(msg: str, print_console=False):
 	with LOGGING_LOCK:
 		if not exists(RESULT_DIR):
 			makedirs(RESULT_DIR)
 
 		with open(join(RESULT_DIR, ".log"), "a+") as logFile:
 			logFile.write(f"[{datetime.datetime.now()}] {msg}\n")
+		
+		if print_console:
+			print(f"[{datetime.datetime.now()}] {msg}")
--- obsrv_plan/general/params.py
+++ obsrv_plan/general/params.py
@@ -12,14 +12,16 @@ from multiprocessing import cpu_count
 # Fedora
 # WORKING_DIR = "/home/ramon/uanl/Tesis/UANL_MAPTA_PlanObservaciones"
 # WORKING_DIR = "/run/media/ramon/T7/Tesis/Gaia Analisis";
-WORKING_DIR = "/home/ramon/uanl/Tesis/Backup Data/Gaia Query Results/sdss-half"
+# WORKING_DIR = "/home/ramon/uanl/Tesis/Backup Data/Gaia Query Results/sdss-half"
+WORKING_DIR = "/home/ramon/uanl/Tesis/Backup Data/Gaia Query Results/sdss-3_4"
 
 # DATA_FILE_PATH = join(WORKING_DIR, "gdr3_sdss_exclusion_phot_cv_candidates-result.csv")
-DATA_FILE_PATH = join(WORKING_DIR, "SDSS Half Photometric Filter GDR3 - Limit-result.csv")
+# DATA_FILE_PATH = join(WORKING_DIR, "SDSS Half Photometric Filter GDR3 - Limit-result.csv")
+DATA_FILE_PATH = join(WORKING_DIR, "SDSS 3_4 Filter GDR3-result.csv")
 
 TIMEZONE = timezone(timedelta(hours=-6))
 OBSV_START_DATETIME = datetime.combine(
-		date(year=2022, month=12, day=6), time(hour=23, minute=0, tzinfo=TIMEZONE))
+		date(year=2024, month=4, day=26), time(hour=21, minute=0, tzinfo=TIMEZONE))
 RESULT_DIR = join(WORKING_DIR, OBSV_START_DATETIME.strftime("%Y%m%d"))
 
 SIMBAD_VOT_RESULTS = join(RESULT_DIR, "simbad-raw_vot")
